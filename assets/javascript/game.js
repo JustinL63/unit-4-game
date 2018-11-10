ec31acb5c0cbe48130e0400db5a07eff0f268657
Justin Lamb <lambjg63@gmail.com>
2018-11-10 17:36:53 -0500

    Getting There

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,11 @@
-var generateGoal = 0;
-var crystalValue = [0,0,0,0];
+var randomGoal = 0;
+var crystalValue = [0, 0, 0, 0];
 var totalScore = 0;
 var wins = 0;
 var losses = 0;
 
-function generate_randomGoal() {
-    generateGoal = Math.floor(Math.Random() * 101) + 19;
+function update_randomGoal() {
+    randomGoal = Math.floor(Math.Random() * 101) + 19;
 }
 
 function return_crystalValue() {
@@ -14,18 +14,23 @@ function return_crystalValue() {
 
 function generate_CrstaylValue() {
     for (let i = 0; i < crystalValue.length; i++) {
-       crystalValue[i] = return_crystalValue();
+        crystalValue[i] = return_crystalValue();
     }
 }
 
 function onClick(buttonClick) {
     totalScore += crystalValue[buttonClick];
+    if (totalScore >= randomGoal) { // game over
+        if (totalScore === randomGoal) //you won this game
+            wins++ //add one win to win tally
+        else losses++ // ad one loss to losses tally
+    }
 }
 
 function new_Game() {
     generate_randomGoal();
     generate_CrstaylValue();
-    updateScreen();    
+    updateScreen();
 }
 
 function updateScreen() {
