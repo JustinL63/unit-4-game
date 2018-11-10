f455812c6faf3eaf5360cc5005556c31b8d28833
Justin Lamb <lambjg63@gmail.com>
2018-11-10 16:28:01 -0500

    Skeleton Code

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+var generateGoal
+var crystalValue
+var totalScore
+var wins
+var losses
+
+function generate_randomGoal() {
+
+}
+
+function generate_CrstaylValue() {
+
+}
+
+function onClick(buttonClick) {
+
+}
+
+function new_Game() {
+    
+}
