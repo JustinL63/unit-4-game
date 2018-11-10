4b00e33da9aee9ff74d2ecce0108e17490d6d9ba
Justin Lamb <lambjg63@gmail.com>
2018-11-10 17:20:18 -0500

    Logical

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,33 @@
-var generateGoal
-var crystalValue
-var totalScore
-var wins
-var losses
+var generateGoal = 0;
+var crystalValue = [0,0,0,0];
+var totalScore = 0;
+var wins = 0;
+var losses = 0;
 
 function generate_randomGoal() {
+    generateGoal = Math.floor(Math.Random() * 101) + 19;
+}
 
+function return_crystalValue() {
+    return Math.floor(Math.Random() * 11) + 1;
 }
 
 function generate_CrstaylValue() {
-
+    for (let i = 0; i < crystalValue.length; i++) {
+       crystalValue[i] = return_crystalValue();
+    }
 }
 
 function onClick(buttonClick) {
-
+    totalScore += crystalValue[buttonClick];
 }
 
 function new_Game() {
-    
+    generate_randomGoal();
+    generate_CrstaylValue();
+    updateScreen();    
+}
+
+function updateScreen() {
+
 }
